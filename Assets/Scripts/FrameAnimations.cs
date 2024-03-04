5b96ec710fb927fc8fc05dd4e93da3a5af5a0efc
mitchelnelson <nelsonm55@mymacewan.ca>
2024-03-03 18:50:33 -0700

    opaque background when won

--- Assets/Scripts/FrameAnimations.cs
+++ Assets/Scripts/FrameAnimations.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class FrameAnimations : MonoBehaviour
 {
     [SerializeField] private Animator animator = null;
+    [SerializeField] private Animator opaqueAnimator = null;
 
     public void PlayAnimations()
     {
         animator.SetTrigger("GameWon");
+        opaqueAnimator.SetTrigger("GameWon");
     }
 }
