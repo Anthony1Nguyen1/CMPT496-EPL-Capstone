5dfd23b90e4d6e23eb0a05b514054293bbc85ff2
Mitchel <mitchelnelson@live.ca>
2024-03-03 20:47:11 -0700

    made a singular win animation controller on canvas

--- Assets/Scripts/WinAnimations.cs
+++ Assets/Scripts/WinAnimations.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FrameAnimations : MonoBehaviour
+public class WinAnimations : MonoBehaviour
 {
     [SerializeField] private Animator animator = null;
-    [SerializeField] private Animator opaqueAnimator = null;
 
     public void PlayAnimations()
     {
         animator.SetTrigger("GameWon");
-        opaqueAnimator.SetTrigger("GameWon");
     }
 }
